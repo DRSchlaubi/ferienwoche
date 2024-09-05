fcb93e775c439b8f8ec5c3e5514939cad6395d74
Michael Rittmeister <michael@rittmeister.in>
2024-09-05 21:51:26 +0200

    Update data

--- app.vue
+++ app.vue
@@ -3,23 +3,23 @@
     <div class="max-w-2xl mx-auto">
       <h1 class="text-3xl font-bold mb-6 text-center">Ferienwochen Information</h1>
       <div class="bg-white dark:bg-gray-900 p-6 rounded-lg shadow-md">
-        <p class="mb-2">Wir haben aktuell die {{ 126481 }}. Ferienwoche nach der Feria Scolarum.</p>
+        <p class="mb-2">Wir haben aktuell die {{ formatNumber(weeksSinceFeriaScolarum) }}. Ferienwoche nach der Feria Scolarum.</p>
         <p class="mb-2">
-          Wir haben aktuell die {{ 4301 }}. Ferienwoche nach <a
+          Wir haben aktuell die {{ formatNumber(weeksSinceSchulferienOrg) }}. Ferienwoche nach <a
             href="https://schulferien.org" target="_blank"
             class="text-blue-500 dark:text-blue-300">Schulferien.org</a>.
         </p>
         <p class="mb-2">
-          Wir haben aktuell die {{ weeksSinceGermanyFoundingDate }}. Ferienwoche seit Gründung der Bundesrepublik
+          Wir haben aktuell die {{ formatNumber(weeksSinceGermanyFoundingDate) }}. Ferienwoche seit Gründung der Bundesrepublik
           Deutschland.
         </p>
         <p class="mb-2">
-          Wir haben aktuell die {{ weeksSinceHamburgTreaty }}. Ferienwoche nach dem „<a
+          Wir haben aktuell die {{ formatNumber(weeksSinceHamburgTreaty) }}. Ferienwoche nach dem „<a
             href="https://de.wikipedia.org/wiki/Hamburger_Abkommen" target="_blank"
             class="text-blue-500 dark:text-blue-300">Hamburger Abkommen</a>“.
         </p>
         <p v-if="currentYear" class="mb-2">
-          Wir haben aktuell die {{ currentYear }}. Ferienwoche des aktuellen Jahres.
+          Wir haben aktuell die {{ formatNumber(currentYear) }}. Ferienwoche des aktuellen Jahres.
         </p>
       </div>
     </div>
@@ -39,6 +39,7 @@
 <script lang="ts">
 import {ChronoUnit, LocalDate, Month, type Temporal} from "@js-joda/core";
 import {findLastSummerHoliday} from './utils/holidays';
+const format = new Intl.NumberFormat();
 
 const currentYear = ref(undefined);
 
@@ -52,8 +53,10 @@ export default {
   data() {
     return {
       currentYear,
-      hamburgTreatyDate: LocalDate.of(1964, Month.OCTOBER, 28),
-      germanyFoundingDate: LocalDate.of(1964, Month.OCTOBER, 28)
+      schulferienOrg: LocalDate.of(1946, Month.JULY, 16),
+      feriaeScolarum: LocalDate.of(-497, Month.DECEMBER, 17),
+      hamburgTreatyDate: LocalDate.of(1965, Month.JULY, 21),
+      germanyFoundingDate: LocalDate.of(1949, Month.JULY, 15)
     };
   },
 
@@ -64,11 +67,22 @@ export default {
     weeksSinceHamburgTreaty(): number {
       return calculateWeeksSince(this.hamburgTreatyDate);
     },
+    weeksSinceFeriaScolarum(): number {
+      return calculateWeeksSince(this.feriaeScolarum);
+    },
+    weeksSinceSchulferienOrg(): number {
+      return calculateWeeksSince(this.hamburgTreatyDate);
+    },
   },
   async mounted() {
     findLastSummerHoliday().then(holiday => {
       currentYear.value = calculateWeeksSince(holiday);
     });
   },
+  methods: {
+    formatNumber(value: number): string {
+      return format.format(value);
+    }
+  }
 }
 </script>
